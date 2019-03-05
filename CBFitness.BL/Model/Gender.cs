28e035bc6b3e88031837c72acfef4bffc0f98cda
timeryanov <aydar.timeryanov@gmail.com>
2019-03-05 16:09:01 +0500

    Создана модель Пользователя.

---
+++ CBFitness.BL/Model/Gender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CBFitness.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создать новый пол.   
+        /// </summary>
+        /// <param name="name">Имя пола.</param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) // if empty
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
+            }
+
+            Name = name;
+        }
+       
+
+        // переопределения метода tostring
+        public override string ToString()
+        {
+            return Name;
+        }
+        
+
+
+    }
+}
---
+++ CBFitness.CMD/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CBFitness.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
